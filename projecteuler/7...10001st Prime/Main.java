46e3ad335a6e604ade9e20a713dd4a9fe3529142
Caden Henrich <henrich.rocking@gmail.com>
2019-09-12 15:13:37 -0400

    Update Main.java

--- projecteuler/7...10001st Prime/Main.java
+++ projecteuler/7...10001st Prime/Main.java
@@ -4,6 +4,31 @@
 public class Main {
 
     public static void main(String[] args) {
+        double primeIndex = 1;
+        double current = 3;
 
+        double maxPrime = 0;
+
+        cloop: while (primeIndex < 10001) {
+            if (current % 2 == 0) {
+                current++;
+                continue;
+            }
+            for (double i = 3; i < current; i += 2) {
+                if (current % i == 0) {
+                    current++;
+                    continue cloop;
+                }
+            }
+
+            System.out.println("Prime: " + current);
+            System.out.println(" Index: " + primeIndex);
+
+            maxPrime = current;
+            current += 2;
+            primeIndex++;
+        }
+
+        System.out.println("\nMaxPrime: " + maxPrime);
     }
 }
