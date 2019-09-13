7a0e3edf5142c1bd6aba41ffae7c55975585a0b5
Caden Henrich <henrich.rocking@gmail.com>
2019-09-13 08:15:12 -0400

    Update Main.java

--- projecteuler/3/Main.java
+++ projecteuler/3/Main.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Main
  */
@@ -8,18 +11,25 @@ public class Main {
         double target = 600851475143.0;
         double largest = 0.0;
 
-        outerloop: for (double i = 0.0; i < target; i++) {
-            if (target % i != 0.0 || i <= largest) {
-                continue;
+        List<Double> factors = new ArrayList<>();
+
+        for (double i = 0.0; i < target; i++) {
+            if (target % i == 0) {
+                factors.add(i);
             }
-            for (double j = 0.0; j < i; j++) {
-                if (i % j == 0.0) {
+        }
+
+        System.out.println(factors);
+
+        outerloop: for (int j = 0; j < factors.size(); j++) {
+            for (double k = 0.0; k < factors.get(j); k++) {
+                if (factors.get(j) % k == 0) {
                     continue outerloop;
                 }
             }
-
-            largest = i;
-            System.out.println(largest);
+            if (factors.get(j) > largest) {
+                largest = factors.get(j);
+            }
         }
 
         System.out.print(largest);
